84c57e1c1849cd3f5a75f9529d4076882fd067c7
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-04-26 21:51:06 +0200

    Testing data preparation and testing it

--- sketch.js
+++ sketch.js
@@ -92,6 +92,62 @@ function trainingDataPreparation () {
   return training
 }
 
+// Runs training
+function trainingNN (nn) {
+  // Getting array with all data to be trained
+  // training now is a 2D array
+  let training = trainingDataPreparation()
+  // Shuffling training array
+  shuffle(training, true)
+  // Training nn for one epoch
+  for (let i = 0; i < training.length; i++) {
+    // Temp array for be used in the nn.train
+    let data = training[i]
+    // Filling inputs one-dimension array
+    let inputs = data.map(x => x / 255)
+    let label = training[i].label
+    // One-hot encoding
+    let targets = [0, 0, 0, 0, 0, 0, 0]
+    targets[label] = 1
+    // Train nn
+    nn.train(inputs, targets)
+  }
+}
+
+// Load testing data
+function testingDataPreparation () {
+  let testing = []
+  testing = testing.concat(cats.testing)
+  testing = testing.concat(dogs.testing)
+  testing = testing.concat(horses.testing)
+  testing = testing.concat(houses.testing)
+  testing = testing.concat(plants.testing)
+  testing = testing.concat(rainbows.testing)
+  testing = testing.concat(trains.testing)
+  return testing
+}
+
+// Runs testing and returns percentage
+function testingNN (nn) {
+  let testing = testingDataPreparation()
+  let correct = 0
+  // Training nn for one epoch
+  for (let i = 0; i < testing.length; i++) {
+    // Temp array for be used in the nn.train
+    let data = testing[i]
+    // Filling inputs one-dimension array
+    let inputs = data.map(x => x / 255)
+    let label = testing[i].label
+    // Testing nn - predict
+    let guess = nn.predict(inputs)
+    // Getting the index of the biggest value
+    let classification = guess.indexOf(max(guess))
+    correct += (classification === label) ? 1 : 0
+  }
+  // Returning percentage of corrects
+  return correct / testing.length
+}
+
 function setup () {
   createCanvas(CANVAS, CANVAS)
   background(51)
@@ -101,33 +157,13 @@ function setup () {
   // 2. 64 base analysis
   // 3. One output for each dataset
   nn = new NeuralNetwork(IMG_SIZE_SQ, 64, 7)
-  // Getting array with all data to be trained
-  // training now is a 2D array
-  let training = trainingDataPreparation()
-  // Shuffling training array
-  shuffle(training, true)
-  // Training nn for one epoch
-  for (let i = 0; i < training.length; i++) {
-    // Temp array for be used in the nn.train
-    let inputs = []
-    let data = training[i]
-
-    // Filling inputs one-dimension array
-    for (var j = 0; j < data.length; j++) {
-      inputs[j] = data[j] / 255
-    }
-
-    let label = training[i].label
-    // One-hot encoding
-    let targets = [0, 0, 0, 0, 0, 0, 0]
-    targets[label] = 1
-
-    // Train nn
-    nn.train(inputs, targets)
+  // Running 5 times
+  for (let i = 0; i < 5; i++) {
+    trainingNN(nn)
+    let percentage = testingNN(nn)
+    print('Round', i + 1)
+    print('Correct: %', percentage)
   }
-
-  print('Trained for one epoch')
-  print(nn)
 
   // Desbuggin data 28x28px
   printRawDataDebugger(cats_data)
